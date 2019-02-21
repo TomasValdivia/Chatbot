3c017f1516782cb45c28fb3b3ef2983c0fa55a87
Developer <devbox.developer@gupshup.io>
2019-02-21 21:50:30 +0000

    Todos los beneficios estan asociados a una url para mayor informacion

---
+++ default.js
@@ -0,0 +1,35 @@
+//ESTA FUNCION PERMITE OBTENER TODOS LOS BENEFICIOS DE LA API
+function beneficiosHandler (options, event, context, callback) {//se crea la funcion
+	context.simplehttp.makeGet ("https://praxair.rubixware.com/api/v1/benefits/", {}, function (context, event) { //hacemos una instancia al API
+	var res = JSON.parse(event.getresp);//obtenemos los datos de la api en json
+	options.data.beneficios0 = res.benefits_categories.benefits[0].title;//accedemos al titulo de cada beneficio
+		//options.data.beneficios0 = res.benefits_categories.benefits[0].id;
+	options.data.beneficios1 = res.benefits_categories.benefits[1].title;
+	options.data.beneficios2 = res.benefits_categories.benefits[2].title;
+	options.data.beneficios3 = res.benefits_categories.benefits[3].title;
+	options.data.beneficios4 = res.benefits_categories.benefits[4].title;
+	options.data.beneficios5 = res.benefits_categories.benefits[5].title;
+	options.data.beneficios6 = res.benefits_categories.benefits[6].title;
+	options.data.beneficios7 = res.benefits_categories.benefits[7].title;
+	options.data.beneficios8 = res.benefits_categories.benefits[8].title;
+	options.data.beneficios9 = res.benefits_categories.benefits[9].title;
+	options.data.beneficios10 = res.benefits_categories.benefits[10].title;
+	
+	callback(options, event, context);
+	});
+}
+
+module.exports.main = {//exportamos los titulos
+    timeLabel: beneficiosHandler
+    //detallesLabel: beneficiosHandler
+}
+
+ //context.simplehttp.makeGet ("https://praxair.rubixware.com/api/v1/benefits/");
+//	var consultar = JSON.parse(event.getresp);
+//	var beneficios = consultar.benefits_categories.benefits;
+//	var resp="";
+//	for (var i = 0; i < beneficios.length; i++) {
+//		resp = resp + beneficios[i].title + "\n";
+//	}
+
+//	context.sendResponse(resp);
