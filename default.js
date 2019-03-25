062a6b7472163b6b90db8a709ae384ea77e0e305
Developer <devbox.developer@gupshup.io>
2019-03-25 17:45:42 +0000

    las ligas de los beneficios funcionan dinamicamente

--- default.js
+++ default.js
@@ -1,25 +1,22 @@
-var links=[['1','https://praxair.rubixware.com/benefits/11'],['2','https://praxair.rubixware.com/benefits/13'],
-		['3','https://praxair.rubixware.com/benefits/1'],['4','https://praxair.rubixware.com/benefits/14'],
-		['5','https://praxair.rubixware.com/benefits/15'],['6','https://praxair.rubixware.com/benefits/18'],
-		['7','https://praxair.rubixware.com/benefits/12'],['8','https://praxair.rubixware.com/benefits/16'],
-		['9','https://praxair.rubixware.com/benefits/19'],['10','https://praxair.rubixware.com/benefits/20'],
-		['11','https://praxair.rubixware.com/benefits/21']];
 //ESTA FUNCION PERMITE OBTENER TODOS LOS BENEFICIOS DE LA API
 function menuHandler (options, event, context, callback) {
 	context.simplehttp.makeGet("https://praxair.rubixware.com/api/v1/benefits/",{},function (context, event) {
 		var beneficios = JSON.parse(event.getresp);//obtenemos todos los beneficios de la API en json
 		var bene = beneficios.benefits_categories.benefits;//accede a los beneficios de la API 
-		var b1="",link="",nombre="";
+		var b1="",link="",nombre="",enumaracion="", beneficio="";
+		var links=[]; 
+		//for para recorrer los links de cada beneficio
 		for (var i=0; i<bene.length; i++) {//bucle para obtener todos los beneficios
-			var con=i+1;
+			var con=i+1; // <------- Aqui esta el indice
+			links.push(con);
 			b1=b1 +con+": "+ bene[i].title + '\n';//obtenemos el titulo de los beneficios
 		}
 		//for para recorrer los links de cada beneficio
-		for(var l = 0; l < links.length; l++){
-			if (event.message == links[l][0]){//if para recibir el messaje de usuario
+		for(var k = 0; k < links.length; k++){
+			if (event.message == links[k]){//if para recibir el messaje de usuario
 				options.next_state='bot1_1';
-				link=link+links[l][1];//obtenemos el link de beneficio
-				nombre=nombre +bene[l].title;//obtenemos titulo de beneficio
+				nombre=nombre +bene[k].title;//obtenemos titulo de beneficio
+				link=link+"https://praxair.rubixware.com/benefits/"+bene[k].id;//obtenemos el link de beneficio
 			}
 		}
 		options.data.title=nombre; //se agrega el titulo a la variable name
@@ -35,10 +32,11 @@ function categoriasHandler (options, event, context, callback) {
 		var bene = beneficios.benefits_categories.benefits;//accede a los beneficios de la API 
 		var category = beneficios.benefits_categories.categories;//accede a las categorias de la API 
 		var benef="",categoria="",name="",link="";
-		var id_categorias = ['1', '2','3','4','5','6','7'];
+		var id_categorias = [];
 		//bucle para obtener el nombre de las categorias
 		for (var i=0; i<category.length; i++) {
 			cont=i+1;//contador
+			id_categorias.push(cont);//<-----guardamos el contador en el arreglo
 			categoria=categoria +cont+": "+ category[i].name +'\n';//nombre categoria
 		}
 		//for para recorrer las categorias
